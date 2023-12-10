9efedb6a9e70df1012d8d78fd9273eab76c93046
Zinnur41 <zagidullin_zin@mail.ru>
2023-12-10 20:06:56 +0300

    update main page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\ReviewService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(ReviewService $reviewService): Response
     {
-        return $this->render('admin/index.html.twig');
+        $averageScore = $reviewService->getAverageScore();
+        return $this->render('admin/index.html.twig', [
+            'averageScore' => $averageScore
+        ]);
     }
 }
